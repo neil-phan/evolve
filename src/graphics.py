d69ac90429de0605da1f6f49a78b7c7c571ce5ce
Alip Arslan <aliparslan@outlook.com>
2022-04-30 22:32:58 -0400

    added terrain stuff to branch

--- src/graphics.py
+++ src/graphics.py
@@ -1,15 +1,20 @@
 import pygame
 from simple import organism, predator, food, tree
+from simple.terrain import simple_terrain
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-#initialize pygame
+# initialize pygame
 pygame.init()
 pygame.font.init()
 WIDTH, HEIGHT = 1200, 1000
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Evolve")
+
+# Terrain variables
+FEATURE_SIZE = 100  # controls "resolution" (higher means more blurry)
+FREQUENCY = 3  # controls noise level (higher means more noise)
 
 # Environment variables
 FPS = 60
@@ -75,9 +80,9 @@ def make_organisms(N):
     env_map = {
         'x_max': WIDTH,
         'y_max': HEIGHT,
-        
-        }
-    for _ in range (N):
+
+    }
+    for _ in range(N):
         color = random_color()
         org = organism.Organism(color, env_map, settings=o_settings)
         orgs.append(org)
@@ -89,9 +94,8 @@ def make_predators(N):
     env_map = {
         'x_max': WIDTH,
         'y_max': HEIGHT,
-        
-        }
-    for _ in range (N):
+    }
+    for _ in range(N):
         color = 'red'
         pred = predator.Predator(color, env_map)
         preds.append(pred)
@@ -106,7 +110,7 @@ def make_foods(N):
         'x_max': WIDTH,
         'y_max': HEIGHT
     }
-    
+
     for _ in range(N):
         f = food.Food(attributes)
         foods.append(f)
@@ -125,7 +129,7 @@ def random_color():
     r = random.randrange(0, 256)
     g = random.randrange(0, 256)
     b = random.randrange(0, 256)
-    return (r,g,b)
+    return (r, g, b)
 
 def make_graph(orgs):
     count = 0
@@ -136,38 +140,38 @@ def make_graph(orgs):
         average_speed += org.speed
         average_size += org.rad
         average_range += org.range
-        count+=1
+        count += 1
     if len(orgs) == 0:
         return 0, 0, 0
     return round(average_speed / count, 2), round(average_size / count, 2), round(average_range / count, 2)
-        
+
 # Draw all the organisms, foods, trees, and statistics
-def draw(orgs, preds, foods, trees, generation_num):
-    WINDOW.fill(BG_COLOR)
+def draw(orgs, preds, foods, trees, generation_num, terrain):
+    WINDOW.blit(terrain, (0, 0))
 
     for org in orgs:
         org.draw(WINDOW)
         org.nearest_food(foods, WIDTH, HEIGHT)
         org.think()
         org.move(WIDTH, HEIGHT)
-        #print(org.wih)
-        
+        # print(org.wih)
+
     for pred in preds:
         pred.draw(WINDOW)
         pred.target_move(orgs, WIDTH, HEIGHT)
 
     for food in foods:
         food.draw(WINDOW)
-    
+
     for tree in trees:
         tree.draw(WINDOW)
-        
+
     # COLLISION DETECTION
     for org in orgs:
         for food in foods:
             if org.is_eating(food):
                 foods.remove(food)
-                
+
     for pred in preds:
         for org in orgs:
             if pred.is_eating(org):
@@ -176,12 +180,13 @@ def draw(orgs, preds, foods, trees, generation_num):
     speed, size, rng = make_graph(orgs)
     np.append(DATA, (speed, size, rng))
     text = FONT.render(f"Generation: {generation_num}", 1, 'black')
-    graph_text = SUB_FONT.render(f"Avg Speed: {speed}| Avg Size: {size} | Avg Range: {rng}", 1, 'black')
-    WINDOW.blit(text, (WIDTH-10-text.get_width(), 10))
-    WINDOW.blit(graph_text, (WIDTH-10-graph_text.get_width(), 50))
+    graph_text = SUB_FONT.render(
+        f"Avg Speed: {speed}| Avg Size: {size} | Avg Range: {rng}", 1, 'black')
+    WINDOW.blit(text, (WIDTH - 10 - text.get_width(), 10))
+    WINDOW.blit(graph_text, (WIDTH - 10 - graph_text.get_width(), 50))
     pygame.display.update()
 
-#create game loop
+# create game loop
 def main():
     clock = pygame.time.Clock()
     running = True
@@ -192,10 +197,12 @@ def main():
     preds = make_predators(INITIAL_PRED_COUNT)
     foods = make_foods(INITIAL_FOOD_COUNT)
     trees = make_trees(INITIAL_TREE_COUNT)
-    
+
     generation_num = 1
+    TERRAIN = simple_terrain(WIDTH, HEIGHT, FREQUENCY)
+
     while running:
-        clock.tick(FPS) #   Caps game frames at desired FPS
+        clock.tick(FPS)  # Caps game frames at desired FPS
         for event in pygame.event.get():
             if event.type == pygame.USEREVENT:
                 counter += 1
@@ -203,60 +210,64 @@ def main():
                 # Update generation num after enough time has passed
                 if counter % GEN_TIMER == 0:
                     generation_num += 1
-                    
+
                 # replenish food in areas without trees
                 if counter % FOOD_REPLENISH_TIME == 0:
                     foods += make_foods(FOOD_REPLENISH_FOOD)
-                    
+
                 # decrease fitness score for every tick
                 for org in orgs:
-                    org.fitness -= O_FITNESS_COST + float(org.rad / O_SIZE_COST)
-                    + float(org.speed / O_SPEED_COST) 
-                    #print(org.fitness)
+                    org.fitness -= O_FITNESS_COST + \
+                        float(org.rad / O_SIZE_COST)
+                    + float(org.speed / O_SPEED_COST)
+                    # print(org.fitness)
                     if org.fitness >= (O_REPRODUCTION_COST
-                                       +(O_LITTER_COST*org.litter_size)-O_LITTER_COST):
+                                       + (O_LITTER_COST * org.litter_size) - O_LITTER_COST):
                         child = org.reproduce()
                         orgs.append(child)
-                        org.fitness -= O_LABOR_COST + (O_LITTER_COST * org.litter_size) - O_LITTER_COST
+                        org.fitness -= O_LABOR_COST + \
+                            (O_LITTER_COST * org.litter_size) - O_LITTER_COST
                     if org.fitness <= O_DEATH:
-                        orgs.remove(org) # DEAD
-                        
+                        orgs.remove(org)  # DEAD
+
                 for pred in preds:
-                    pred.fitness -= P_FITNESS_COST + float(pred.rad / P_SIZE_COST) 
+                    pred.fitness -= P_FITNESS_COST + \
+                        float(pred.rad / P_SIZE_COST)
                     + float(pred.speed / P_SPEED_COST)
                     if pred.fitness >= (P_REPRODUCTION_COST
                                         + (P_LITTER_COST * pred.litter_size) - P_LITTER_COST):
                         child = pred.reproduce()
                         preds.append(pred)
-                        pred.fitness -= P_LABOR_COST + (P_LITTER_COST * pred.litter_size) - P_LITTER_COST
+                        pred.fitness -= P_LABOR_COST + \
+                            (P_LITTER_COST * pred.litter_size) - P_LITTER_COST
                     if pred.fitness <= P_DEATH:
-                        preds.remove(pred) # DEAD
-                    
-                        
+                        preds.remove(pred)  # DEAD
+
                 # Every 5 seconds trees spawn food within its radius
                 if counter % TREE_REPLENISH_TIME == 0:
                     for tree in trees:
                         attributes = {
-                            'x_min': tree.x - (3*tree.rad),
-                            'y_min': tree.y - (3*tree.rad),
-                            'x_max': tree.x +(3*tree.rad),
-                            'y_max': tree.y + (3*tree.rad),
+                            'x_min': tree.x - (3 * tree.rad),
+                            'y_min': tree.y - (3 * tree.rad),
+                            'x_max': tree.x + (3 * tree.rad),
+                            'y_max': tree.y + (3 * tree.rad),
                         }
                         for _ in range(TREE_REPLENISH_FOOD):
                             f = food.Food(attributes, energy_max=ENERGY_MAX)
                             foods.append(f)
-                            
+
                 # Spoiling food to avoid having clusters of food if no organisms contest it
                 for f in foods:
                     f.decay += 1
                     if f.decay == FOOD_SPOIL:
                         foods.remove(f)
-      
+
             if event.type == pygame.QUIT:
                 running = False
-                           
+
         # Display all objects on screen
-        draw(orgs, preds, foods, trees, generation_num)
+        draw(orgs, preds, foods, trees, generation_num, TERRAIN)
+
 
 if __name__ == '__main__':
     main()
---
+++ src/simple/terrain.py
@@ -0,0 +1,113 @@
+from PIL import Image
+import opensimplex as simplex
+import random
+import pygame
+
+# initialize biome colors
+WATER = (0, 0, 255)
+BEACH = (249, 209, 153)
+FOREST = (144, 169, 85)
+JUNGLE = (79, 119, 45)
+SAVANNAH = (49, 87, 44)
+DESERT = (236, 243, 158)
+SNOW = (240, 234, 210)
+
+# randomizing seed of generator
+simplex.seed(random.randrange(0, 10000))
+
+"""
+Each of the terrain generation functions returns a pygame-usable image.
+This can be mapped onto the background.
+"""
+
+# generates a noise value
+def noise(nx, ny):
+    return simplex.noise2(nx, ny) / 2.0 + 0.5
+
+# semi-limited but simple terrain generation
+def simple_terrain(width, height, frequency):
+    im = Image.new('RGB', (width, height))
+    for y in range(0, height):
+        for x in range(0, width):
+            nx = x / width - 0.5
+            ny = y / height - 0.5
+            # value = noise(FREQUENCY * nx, FREQUENCY * ny)
+            e = 1 * noise(frequency * 1 * nx, frequency * 1 * ny)
+            + 0.5 * noise(frequency * 2 * nx, frequency * 2 * ny)
+            + 0.25 * noise(frequency * 4 * nx, frequency * 4 * ny)
+            value = e / (1 + 0.5 + 0.25)
+            if value < 0.1:
+                color = WATER
+            elif value < 0.2:
+                color = BEACH
+            elif value < 0.3:
+                color = FOREST
+            elif value < 0.5:
+                color = JUNGLE
+            elif value < 0.7:
+                color = SAVANNAH
+            elif value < 0.9:
+                color = DESERT
+            else:
+                color = SNOW
+            # color = (int(value * 256), int(value * 256), int(value * 256))
+            # color = int((value + 1) * 128)
+            im.putpixel((x, y), color)
+    im.save('landscape.png')
+    terrain = pygame.image.load('landscape.png')
+    return terrain
+
+# gives much more control over how terrain is generated
+def complex_terrain(width, height):
+    im = Image.new('RGB', (width, height))
+    genE = simplex.OpenSimplex(random.randrange(0, 1000))
+    genM = simplex.OpenSimplex(random.randrange(0, 1000))
+
+    def noiseE(nx, ny):
+        return genE.noise2(nx, ny) / 2 + 0.5
+
+    def noiseM(nx, ny):
+        return genM.noise2(nx, ny) / 2 + 0.5
+    for y in range(0, height):
+        for x in range(0, width):
+            nx = x / width - 0.5
+            ny = y / height - 0.5
+            e = (0.56 * noiseE(1 * nx, 1 * ny)
+                 + 0.42 * noiseE(2 * nx, 2 * ny)
+                 + 0.27 * noiseE(4 * nx, 4 * ny)
+                 + 0.13 * noiseE(8 * nx, 8 * ny)
+                 + 0.06 * noiseE(16 * nx, 16 * ny)
+                 + 0.03 * noiseE(32 * nx, 32 * ny))
+            e = e / (0.56 + 0.42 + 0.27 + 0.13 + 0.06 + 0.03)
+            value = e ** 4.80
+            if abs(value) < 0.005:
+                color = WATER
+            elif abs(value) < 0.02:
+                color = BEACH
+            elif abs(value) < 0.03:
+                color = FOREST
+            elif abs(value) < 0.05:
+                color = JUNGLE
+            elif abs(value) < 0.07:
+                color = SAVANNAH
+            elif abs(value) < 0.09:
+                color = DESERT
+            else:
+                color = SNOW
+            im.putpixel((x, y), color)
+    im.save('terrain.png')
+    terrain = pygame.image.load('terrain.png')
+    return terrain
+
+# greyscale barebones terrain generation
+def bastard(width, height, feature):
+    im = Image.new('L', (width, height))
+    for y in range(0, height):
+        for x in range(0, width):
+            value = abs(simplex.noise2(x / feature, y / feature))
+            color = int((value + 1) * 128)
+            im.putpixel((x, y), color)
+    im.save('terrain.png')
+    im.show()
+    terrain = pygame.image.load('terrain.png')
+    return terrain
